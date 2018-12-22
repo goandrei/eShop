ce684877fa3d7ff18725b647e416504c22953487
Andrei <andreigorneanu@gmail.com>
2018-12-22 18:59:54 +0200

    add functionality for editing and deleting an item

--- WebApplication1/Controllers/ShopController.cs
+++ WebApplication1/Controllers/ShopController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using PagedList;
 using WebApplication1.Models.Data;
+using WebApplication1.Models.ViewModels.Categories;
 using WebApplication1.Models.ViewModels.Items;
 
 namespace WebApplication1.Controllers
@@ -40,6 +41,67 @@ namespace WebApplication1.Controllers
             ViewBag.OnePageOfItems = onePageOfItems;
             //return view with list
             return View(itemList);
+        }
+
+        [HttpGet]
+        public ActionResult EditItem(int id)
+        {
+            ItemsVM item;
+
+            using (Db db = new Db())
+            {
+
+                ItemsDTO dto = db.Items.Find(id);
+                if (dto == null)
+                {
+                    return Content("The item doesn't exist");
+                }
+
+                item = new ItemsVM(dto);
+
+                List<CategoryVM> categories;
+
+                item.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult EditItem(ItemsVM item)
+        {
+            using (Db db = new Db())
+            {
+                ItemsDTO dto = db.Items.Find(item.Id);
+
+                dto.Id = item.Id;
+                dto.Title = item.Title;
+                dto.Description = item.Description;
+                dto.Price = item.Price;
+
+                CategoriesDTO catDto = db.Categories.FirstOrDefault(x => x.Id == item.CategoryId);
+                dto.CategoryName = catDto.Name;
+
+                db.SaveChanges();
+
+                TempData["Status"] = "Item edited! <3";
+            }
+
+            return RedirectToAction("Items");
+        }
+
+        public ActionResult DeleteItem(int id)
+        {
+            using (Db db = new Db())
+            {
+                ItemsDTO dto = db.Items.Find(id);
+
+                //remove the item
+                db.Items.Remove(dto);
+                db.SaveChanges();
+
+                return RedirectToAction("Items");
+            }
         }
     }
 }
